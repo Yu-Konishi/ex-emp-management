ed59f33aa3d3466aadf971ebd3a01406ee9290fb
Yu-Konishi <yu.konishi@rakus.co.jp>
2020-06-17 10:25:17 +0900

    ドメインクラスとリポジトリクラスが完成しました。
    管理者情報を表すドメインを作成
    
    従業員情報を表すドメインを作成
    
    administratorsを操作するリポジトリを作成
    
    employeesテーブルを操作するリポジトリを作成

---
+++ src/main/java/jp/co/sample/repository/AdministratorRepository.java
@@ -0,0 +1,69 @@
+package jp.co.sample.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import jp.co.sample.domain.Administrator;
+
+/**
+ * administratorsを操作するリポジトリ(DAO)
+ * @author yu.konishi
+ *
+ */
+@Repository
+public class AdministratorRepository {
+
+	/**
+	 * Administratorオブジェクトを生成
+	 */
+	private static final RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER = (rs, i) -> {
+		Administrator administrator = new Administrator();
+		administrator.setId(rs.getInt("id"));
+		administrator.setName(rs.getString("name"));
+		administrator.setMailAddress(rs.getString("mail_address"));
+		administrator.setPassword(rs.getString("password"));
+		return administrator;
+	};
+
+	/**
+	 * JDBCを操作するテンプレート
+	 */
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	/**
+	 * 管理者情報を挿入する
+	 * @param administrator 管理者情報
+	 */
+	public void insert(Administrator administrator) {
+		SqlParameterSource param = new BeanPropertySqlParameterSource(administrator);
+		String sql = "INSERT INTO administrators(name,mail_address,password) "
+				+ "VALUES (:name,:mailAddress,password) WHERE id=:id;";
+		template.update(sql, param);
+	}
+
+	/**
+	 * メールアドレスとパスワードから管理者情報を取得する(1件も存在しない場合はnullを返す)
+	 * @param mailAddress メールアドレス
+	 * @param password パスワード
+	 * @return 該当した管理者情報(1件も存在しない場合はnull)
+	 */
+	public Administrator findByMailAddressAndPassword(String mailAddress, String password) {
+		String sql = "SELECT id,name,mail_address,password FROM administrators WHERE mail_address=:mailAddress AND password=:password;";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress", mailAddress).addValue("password",
+				password);
+		List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
+		if (administratorList.size() == 0) {
+			return null;
+		}
+		return administratorList.get(0);
+	}
+
+}
---
+++ src/main/java/jp/co/sample/repository/EmployeeRepository.java
@@ -0,0 +1,86 @@
+package jp.co.sample.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import jp.co.sample.domain.Employee;
+
+/**
+ * employeesを操作するリポジトリ(DAO)
+ * @author yu.konishi
+ *
+ */
+@Repository
+public class EmployeeRepository {
+
+	/**
+	 * Employeeオブジェクトを生成
+	 */
+	private static final RowMapper<Employee> EMPLOYEE_ROW_MAPPER = (rs, i) -> {
+		Employee employee = new Employee();
+		employee.setId(rs.getInt("id"));
+		employee.setName(rs.getString("name"));
+		employee.setImage(rs.getString("image"));
+		employee.setGender(rs.getString("gender"));
+		employee.setHireDate(rs.getTimestamp("hire_date"));
+		employee.setMailAddress(rs.getString("mail_address"));
+		employee.setZipCode(rs.getString("zip_code"));
+		employee.setAddress(rs.getString("address"));
+		employee.setTelephone(rs.getString("telephone"));
+		employee.setSalary(rs.getInt("salary"));
+		employee.setCharacteristics(rs.getString("characteristics"));
+		employee.setDependentsCount(rs.getInt("dependents_count"));
+		return employee;
+	};
+
+	/**
+	 * JDBCを操作するテンプレート
+	 */
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+
+	
+	/**
+	 * 従業員一覧情報を入社日順で取得する(従業員が存在しない場合はサイズ0件の従業員一覧を返す)
+	 * @return 該当した従業員情報のリスト
+	 */
+	public List<Employee> findAll() {
+		String sql = "SELECT id,name,image,gender,hire_date,mail_address,zip_code,address,telephone,salary,"
+				+ "characteristics,dependents_count FROM employees ORDER BY hire_date DESC;";
+		List<Employee> employeeList = template.query(sql, EMPLOYEE_ROW_MAPPER);
+		return employeeList;
+	}
+
+	/**
+	 * 主キーから従業員情報を取得する(従業員が存在しない場合はSpringが自動的に例外を発生します)
+	 * @param id ID
+	 * @return 該当する従業員情報
+	 */
+	public Employee load(Integer id) {
+		String sql = "SELECT id,name,image,gender,hire_date,mail_address,zip_code,address,telephone,salary,"
+				+ "characteristics,dependents_count FROM employees WHERE id=:id";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		Employee employee = template.queryForObject(sql, param, EMPLOYEE_ROW_MAPPER);
+		return employee;
+	}
+
+	/**
+	 * 従業員情報を変更する(idカラムは更新しない)
+	 * @param employee
+	 */
+	public void update(Employee employee) {
+		String sql = "UPDATE employees SET name=:name,image=:image,gender=:gender,hire_date=:hireDate,"
+				+ "mail_address=:mailAddress,zip_code=:zipCode,address=:address,telephone=:telephone,salary=:salary,"
+				+ "characteristics=:characteristics,dependents_count=:dependendsCount WHERE id=:id";
+		SqlParameterSource param = new BeanPropertySqlParameterSource(employee);
+		template.update(sql, param);
+	}
+
+}
